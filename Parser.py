cf5e84e03116fde30f7108ccce70ef96782d6880
abdoo5xp <45048241+abdoo5xp@users.noreply.github.com>
2020-05-07 07:28:36 +0200

    Add files via upload

---
+++ Parser.py
@@ -0,0 +1,89 @@
+import exc as ex 
+
+symbol_table = {}
+
+def parse(toks):
+	i = 0
+	#print(toks)
+	while(i < len(toks)):
+		if toks[i] == "PRINT":          
+			if toks[i+1][0:6] =="STRING":      #here i am printing only strings 
+				#print("FOUND STRING ")
+				string = getVal(toks[i+1])
+				
+				# if "%d" in string:
+				# 	clean_string = ""        #Initialize result string
+				# 	for character in string:
+				# 		if character.isalnum():        #a function that allows only characters and digits
+				# 			clean_string += character   # Add alphanumeric characters
+				# 	print(clean_string)
+				# 	string += getVal(toks[i+2])
+				# 	i+=1
+
+				print(string)
+			elif toks[i+1][0:3] =="VAR":
+				print(getVal(toks[i+1]))
+			i+=2		
+		elif toks[i][0:3] == "NUM":    
+			#print(toks[i][4:])
+			getVal(toks[i])
+			i+=1
+		elif toks[i][0:3] == "EXP": 
+			#print(toks[i][4:])
+			getVal(toks[i])
+			i+=1
+		elif toks[i][0:3] == "VAR":  # this is the token --> 'VAR:$var_name''EQUALS''TYPE:var_value'
+			if toks[i+1] == "EQUALS":       
+				if toks[i+2][0:3] in "EXP": 
+					makeVar(toks[i][5:], "NUM:"+ getVal(toks[i+2])) # here i didn't use str(getval(toks[i+2])) cause the return of my getval is a string 
+				elif toks[i+2][0:3] in "NUM":
+					makeVar(toks[i][5:], toks[i+2])
+				elif toks[i+2][0:6] == "STRING":
+					makeVar(toks[i][5:],toks[i+2])  					#print(toks[i][5:]+" "+ "\"" + toks[i+2][7:] + "\"") # i have added the "" to the string back, and removed this $ 	
+				elif toks[i+2][0:3] in "VAR": 
+					makeVar(toks[i][5:],getVal(toks[i+2]))
+			i+=3
+		elif toks[i] == "INPUT":  # input STRING:"sth" VAR:$var_name
+			if toks[i+1][0:6] == "STRING" and toks[i+2][0:3] == "VAR": # i was gonna make another if inside for string and other types but python saves input as a string so screw it.                
+				getInput(getVal(toks[i+1]),toks[i+2][5:])	
+			i+=3
+		# elif toks[i] == "IF":  # input STRING:"sth" VAR:$var_name
+		# 	if toks[i+1][0:3] is "VAR":
+		# 		if toks[i+2][0:6] == "STRING":
+		# 			getInput(getVal(toks[i+1]),toks[i+2][5:])	
+		# 	i+=3 
+	print("my symbol table :",symbol_table)
+	
+
+def getInput(string_to_be_printed, varname):
+	i ="STRING:"+ input(string_to_be_printed +" ") # i tried python and it saves it as a STRING 
+	makeVar(varname,i)
+
+
+def makeVar(varname,varvalue):
+	symbol_table[varname] = varvalue
+
+
+def getVar(varname):
+	if varname in symbol_table:	
+			return symbol_table[varname]
+	else:
+		print("undefined variable")
+		exit()
+
+def getVal(token):
+	if token[0:6]=="STRING":
+		token = token[7:]
+		#token = token[-1]
+	elif token[0:3] == "NUM":
+		token = token[4:]
+	elif token[0:3] == "EXP":
+		#print("EXP is here ")
+		token = ex.evaluate(ex.evalExp(token[4:]))
+		#token = token[-1]
+	elif token[0:3] == "VAR":  #'VAR:$var_name''EQUALS''TYPE:var_value'
+		name = token[5:]   # this is the variable name that i wanna get its value from its table 
+		token = getVar(name)
+		token = getVal(token)       # i am awesome <3 <3 
+	#print(token)
+	return token
--- lexer.py
+++ lexer.py
@@ -5,8 +5,11 @@ def lex(filecontents):
 	string = ""
 	num = ""
 	exp = ""
+	condition = ""
+	iscomment = 0
 	state = 0    # state a flage to check of this tok is inside a string or not 
 	isexp = 0
+	iscondition = 0
 	varstarted = 0
 	var = ""     
 	special_characters = "~!@#$%^&*()+/*-`<>"
@@ -14,10 +17,10 @@ def lex(filecontents):
 	#print(filecontents)
 	for char in filecontents:
 		tok += char
-		if tok ==" ":
+		if tok in " \t":
 			if state == 0:
 				tok = ""
-		elif tok == "\n" or tok == "<EOF>":  # to read multiple lines 
+		elif tok == "\n" or tok == "<EOF>" :  # to read multiple lines 
 			if exp != "" and isexp == 1:
 				#print(exp + "Exp")
 				tokens.append("EXP:" + exp)
@@ -31,9 +34,34 @@ def lex(filecontents):
 				varstarted = 0  
 				var = ""
 			tok=""
+			iscomment = 0
+		elif tok is "#":
+			iscomment = 1
+		elif iscomment:
+			tok=""
 		elif tok.upper() == "PRINT":       #To take upper and lower case "print"
 			tokens.append("PRINT")
 			tok=""
+		elif tok.upper() == "INPUT":       #To take upper and lower case "input"
+			tokens.append("INPUT")
+			tok=""
+		elif tok.upper() == "IF":       #To take upper and lower case "input"
+			tokens.append("IF")
+			#iscondition = 1          lets continue in another time 
+			tok=""
+		elif tok.upper() == "THEN":       #To take upper and lower case "input"
+			if exp != "" and isexp == 0:
+				#print(exp +"num")
+				tokens.append("NUM:" + exp)
+				exp = ""
+			tokens.append("THEN")
+			tok=""
+		elif tok.upper() == "ENDIF":       #To take upper and lower case "input"
+			tokens.append("ENDIF")
+			tok=""
+		# elif iscondition:
+		# 	condition += tok
+		# 	tok=""
 		elif tok in "0123456789" and varstarted == 0: #i have added this so we can variable names can have numbers 
 			exp += tok  
 			tok = ""
@@ -42,11 +70,18 @@ def lex(filecontents):
 			exp += tok
 			tok = ""
 		elif tok == "=" and state == 0:
+			if exp != "" and isexp == 0:
+				#print(exp +"num")
+				tokens.append("NUM:" + exp)
+				exp = ""
 			if var != "":               
 				tokens.append("VAR:" + var)    
 				varstarted = 0  
 				var = ""
-			tokens.append("EQUALS")
+			if tokens[-1] is "EQUALS":
+				tokens[-1] = "EQEQ"
+			else:
+				tokens.append("EQUALS")
 			tok = ""
 		elif tok == "$" and state == 0:
 			varstarted = 1
@@ -64,7 +99,7 @@ def lex(filecontents):
 			#print(var)
 			#print("the tok ",tok)
 			tok = "" 
-		elif tok == "\"":
+		elif tok in " \"":
 			if state == 0:    # this is state for telling the compiler this is the first of the string start saving characters 
 				state = 1
 				tok = ""       # i have added this to not add the " with the string
@@ -75,8 +110,9 @@ def lex(filecontents):
 				tok = ""
 		elif state == 1: 
 			string += tok  # saving the string  
-			tok = ""	
+			tok = ""
+			
 	#print (exp)
 	print(tokens)
 	return tokens
-
+	#return ''
